3e316bffe80b24c0600500a9e6182d3b02053f3b
Charlie Powell <powellcj12@gmail.com>
2014-05-10 14:35:31 -0700

    Add some pseudocode for scrambleWord implementation

--- src/Scramble.java
+++ src/Scramble.java
@@ -7,7 +7,18 @@ public class Scramble {
 	}
 	
 	public static String scrambleWord(String word) {
-		return null;
+		String upperCaseWord = word.toUpperCase();
+		String scrambledWord = "";
+		
+		int i = 0;
+		while(i < upperCaseWord.length()) {
+			// scramble this current and next letter if applicable
+			
+			// otherwise check the next set of letters
+			i++;
+		}
+		
+		return scrambledWord;
 	}
 	
 	public static void scrambleOrRemove(List<String> wordList) {
